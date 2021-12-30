8fefcc84310647c3c25a6d4e56644e04b0c8de87
Bruce-Hopkins-Jr <behopkinsjr@gmail.com>
2021-12-30 09:34:13 +0100

    Fix: CLI  interface

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 // Cargo
-use std::io;
 use std::env;
 
 
@@ -18,63 +17,67 @@ async fn main() {
     for i in 1..input_vec.len() {
         args.push(input_vec[i].clone());
     }
-    match args[1].as_str() {
+    match args[0].as_str() {
         "benchmark" => {
+            if args.len() < 2 {
+                println!("Please insert a url");
+                return;
+            }  
             let mut tasks:i32 = 2;
             let mut connections:i32 = 4;
             let mut time_limit:u64 = 10;
             let mut did_fail:bool = false;
+            
+            if args.len() >= 2 {
+                for i in 2..args.len() {
 
-
-            for i in 2..args.len() {
-
-                if args[i].contains("-t") {
-
-                    let inputed_tasks =  args[i].replace("-t", "");
-                    tasks = match inputed_tasks.clone().parse() {
-                        Ok(result) => result,
-                        Err(_) => {
-                            println!("Tasks were not created. {} is not a number.", inputed_tasks);
-                            did_fail = true;
-                            2
+                    if args[i].contains("-t") {
+                        let inputed_tasks =  args[i].replace("-t", "");
+                        tasks = match inputed_tasks.clone().parse() {
+                            Ok(result) => result,
+                            Err(_) => {
+                                println!("Tasks were not created. {} is not a number.", inputed_tasks);
+                                did_fail = true;
+                                2
+                            }
+                        }
+                    }
+                    if args[i].contains("-c") {
+                        let inputed_tasks =  args[i].replace("-c", "");
+                        connections = match inputed_tasks.clone().parse() {
+                            Ok(result) => result,
+                            Err(_) => {
+                                println!("Connections were not created. {} is not a number.", inputed_tasks);
+                                did_fail = true;
+                                2
+                            }
+                        }
+                    }
+                    if args[i].contains("-d") {
+                        let inputed_tasks =  args[i].replace("-d", "");
+                        time_limit = match inputed_tasks.clone().parse() {
+                            Ok(result) => result,
+                            Err(_) => {
+                                println!("Duration were not created. {} is not a number.", inputed_tasks);
+                                did_fail = true;
+                                2
+                            }
                         }
                     }
                 }
-                if args[i].contains("-c") {
-                    let inputed_tasks =  args[i].replace("-c", "");
-                    connections = match inputed_tasks.clone().parse() {
-                        Ok(result) => result,
-                        Err(_) => {
-                            println!("Tasks were not created. {} is not a number.", inputed_tasks);
-                            did_fail = true;
-                            2
-                        }
-                    }
+            } 
+            match did_fail {
+                true => {}
+                false => {
+                    let url = args[1].clone();
+                    benchmark(url, tasks, connections, time_limit).await;
                 }
-                if args[i].contains("-d") {
-                    let inputed_tasks =  args[i].replace("-d", "");
-                    time_limit = match inputed_tasks.clone().parse() {
-                        Ok(result) => result,
-                        Err(_) => {
-                            println!("Tasks were not created. {} is not a number.", inputed_tasks);
-                            did_fail = true;
-                            2
-                        }
-                    }
-                }
-                match did_fail {
-                    true => {}
-                    false => {
-                        benchmark(String::from("http://localhost:5000/api/posts/618045a63a5cc9120c2a855b"), tasks, connections, time_limit).await;
-                    }
-                }
-
-
             }
+
         
 
         },
-        _ => {println!("Command is not understood")} 
+        other => {println!("Command is not understood: {}", &other)} 
     }
 
     
